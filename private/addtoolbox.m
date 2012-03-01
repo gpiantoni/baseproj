91b38f5bed5adb60c1ab1c3b9c172ddf648f46ba
Gio <github@gpiantoni.com>
2012-03-01 17:03:29 +0100

    addtoolbox adds paths only if they exists, more flexible

--- private/addtoolbox.m
+++ private/addtoolbox.m
@@ -1,42 +1,27 @@
 function output = addtoolbox(cfg)
 % add various toolbox:
 %   - fieldtrip
+%
 %   - gtoolbox
+%
 %   - eventbased
-%   - project specific
+%   - mri2lead
+%   - detectsleep
+%
+%   - project specific (with subdirectories)
+% The project-specific folder should be called [cfg.cond '_private']
 
 %-------------------------------------%
-%-toolbox names
-%-------%
+%-FIELDTRIP (always necessary)
 ftpath = '/data1/toolbox/fieldtrip/'; % fieldtrip (svn)
-gpath = [cfg.scrp 'gtoolbox/']; % gtoolbox (bitbucket, with subdirectories)
-epath = [cfg.scrp 'eventbased/']; % eventbased toolbox (github)
-ppath = [cfg.scrp cfg.proj '_private/']; % project private (github)
-%-------%
 
 %-----------------%
 %-addpath
 addpath(ftpath) 
 ft_defaults
 addpath([ftpath 'qsub/'])
-
-%-------%
-%-add gtool toolbox (here, otherwise matlab does not recognize "import"
-% statement in subfunctions) and remove matlab_bgl, because one function
-% has the same name as a built-in functions, giving tons of warnings
-oldpath = genpath(gpath);
-dirs = regexp(oldpath, ':', 'split');
-goodpath = dirs(cellfun(@isempty, regexp(dirs, 'matlab_bgl')));
-addpath(sprintf('%s:', goodpath{:}))
-%-------%
-
-addpath(epath)
-addpath(ppath)
 %-----------------%
-%-------------------------------------%
 
-%-------------------------------------%
-%-keep track of version
 %-----------------%
 %-get fieldtrip version
 try % so many thing can go wrong here
@@ -44,42 +29,68 @@ try % so many thing can go wrong here
 catch ME
   ftver = ME.message;
 end
-output = sprintf('fieldtrip:\t\t%s', ftver);
+output = sprintf('fieldtrip:\t%s', ftver);
 %-----------------%
+%-------------------------------------%
 
-%-----------------%
-%-get gtoolbox version
-gtool = dir(gpath);
-for i = 3:numel(gtool)
-  try % so many thing can go wrong here
-    [~, gver] = system(['hg --debug tags --cwd ' gpath gtool(i).name ' | awk ''{print $2}''']);
-  catch ME
-    gver = ME.message;
+%-------------------------------------%
+%-GTOOLBOX
+gpath = [cfg.scrp 'gtoolbox/']; % gtoolbox (bitbucket, with subdirectories)
+
+if isdir(gpath)
+  %-----------------%
+  %-add gtool toolbox (here, otherwise matlab does not recognize "import"
+  % statement in subfunctions) and remove matlab_bgl, because one function
+  % has the same name as a built-in functions, giving tons of warnings
+  oldpath = genpath(gpath);
+  dirs = regexp(oldpath, ':', 'split');
+  goodpath = dirs(cellfun(@isempty, regexp(dirs, 'matlab_bgl')));
+  addpath(sprintf('%s:', goodpath{:}))
+  %-----------------%
+  
+  %-----------------%
+  %-get gtoolbox version
+  gtool = dir(gpath);
+  for i = 3:numel(gtool)
+    try % so many thing can go wrong here
+      [~, gver] = system(['hg --debug tags --cwd ' gpath gtool(i).name ' | awk ''{print $2}''']);
+    catch ME
+      gver = ME.message;
+    end
+    outtmp = sprintf('gtoolbox %s:\t%s', gtool(i).name, gver);
+    output = [output outtmp];
   end
-  outtmp = sprintf('gtoolbox %s:\t%s', gtool(i).name, gver);
+  %-----------------%
+end
+%-------------------------------------%
+
+%-------------------------------------%
+%-POTENTIAL TOOLBOXES
+%-----------------%
+%-check which toolboxes are present
+toolbox = {'eventbased' 'detectsleep' 'mri2lead' [cfg.cond '_private']};
+dirtools = dir(cfg.scrp);
+toolbox = intersect(toolbox, {dirtools.name}); % only those that are present
+%-----------------%
+
+%-----------------%
+%-add present toolbox
+for i = 1:numel(toolbox)
+  
+  tpath = [cfg.scrp toolbox{i} filesep];
+  addpath(genpath(tpath)) % with subdirectories
+  
+  %-------%
+  %-get git version
+  try % so many thing can go wrong here
+    [~, tver] = system(['git --git-dir=' tpath '.git log |  awk ''NR==1'' | awk ''{print $2}''']);
+  catch ME
+    tver = ME.message;
+  end
+  outtmp = sprintf('%s:\t%s', toolbox{i}, tver);
   output = [output outtmp];
+  %-------%
+  
 end
-%-----------------%
-
-%-----------------%
-%-get eventbased version
-try % so many thing can go wrong here
-  [~, ever] = system(['git --git-dir=' epath '.git log |  awk ''NR==1'' | awk ''{print $2}''']);
-catch ME
-  ever = ME.message;
-end
-outtmp = sprintf('eventbased:\t\t%s', ever);
-output = [output outtmp];
-%-----------------%
-
-%-----------------%
-%-get project_private version
-try % so many thing can go wrong here
-  [~, pver] = system(['git --git-dir=' ppath '.git log |  awk ''NR==1'' | awk ''{print $2}''']);
-catch ME
-  pver = ME.message;
-end
-outtmp = sprintf('%s_private:\t\t%s', cfg.proj, pver);
-output = [output outtmp];
 %-----------------%
 %-------------------------------------%
